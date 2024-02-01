565972eb94e880cf2ccbc6b3bc893c3118576da5
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-02-01 22:41:03 +0600

    Tried API

--- script.js
+++ script.js
@@ -25,3 +25,72 @@ function picShow()
 {
     document.getElementById("pic").className = "show";
 }
+
+
+function showQuote()
+{
+    const url = 'https://v2.jokeapi.dev/joke/Any?type=single';
+
+try {
+	fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        alert(data.joke);
+    });
+} catch (error) {
+	alert(error);
+}
+}
+
+function showCoffee()
+{
+    const url = "https://coffee.alexflipnote.dev/random.json";
+    try
+    {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById("pic").src = data.file;
+        });
+    }
+    catch(error)
+    {
+        alert(error);
+    }
+}
+
+function showBurger()
+{
+    const url = "https://foodish-api.com/images/burger";
+
+    try
+    {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById("pic").src = data.image;
+        });
+    }
+    catch(error)
+    {
+        alert(error);
+    }
+}
+
+function showNew(type1, type2)
+{
+    alert(document.getElementById(type1).value+" "+document.getElementById(type2).value);
+    if (document.getElementById(type1).value === "on")
+    {
+        showCoffee();
+    }
+    else if(document.getElementById(type2).value === "on")
+    {
+        showBurger();
+    }
+    else
+    {
+        alert("Hi");
+        document.getElementById("pic").src = "icon.jpg";
+    }
+}
